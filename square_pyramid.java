afb4932e62eea22ad658383fee2024d85dba2174
amitbishnoii <amitbishnoi29012@gmail.com>
2025-09-21 22:11:33 +0530

    small bug fixed

--- square_pyramid.java
+++ square_pyramid.java
@@ -12,9 +12,10 @@ public class square_pyramid {
                 }
                 System.out.println();
             }
+
         } else {
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < 7; j++) {
+                for (int j = 0; j < n; j++) {
                     System.out.print(n);
                 }
                 System.out.println();
