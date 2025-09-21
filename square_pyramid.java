c8fea61a8711d8e19823fb9c50713f3d974381e5
amitbishnoii <amitbishnoi29012@gmail.com>
2025-09-21 22:09:18 +0530

    created a program to print square patterns

---
+++ square_pyramid.java
@@ -0,0 +1,25 @@
+
+public class square_pyramid {
+    public static void main(String[] args) {
+        print_pattern(5);
+    }
+
+    public static void print_pattern(int n) {
+        if (n == 4) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < 4; j++) {
+                    System.out.print(n);
+                }
+                System.out.println();
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < 7; j++) {
+                    System.out.print(n);
+                }
+                System.out.println();
+            }
+        }
+
+    }
+}
