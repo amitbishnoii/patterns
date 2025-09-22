7894f7fa16bd05f7076f3a4e0d830538373d29b7
amitbishnoii <amitbishnoi29012@gmail.com>
2025-09-22 15:27:42 +0530

    added print_pattern_2 function to print the star pyramid pattern

--- square_pyramid.java
+++ square_pyramid.java
@@ -1,13 +1,14 @@
 
 public class square_pyramid {
     public static void main(String[] args) {
-        print_pattern(5);
+        // print_pattern(10);
+        print_pattern_2(5);
     }
 
     public static void print_pattern(int n) {
         if (n == 4) {
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < 4; j++) {
+                for (int j = 0; j < n; j++) {
                     System.out.print(n);
                 }
                 System.out.println();
@@ -22,5 +23,14 @@ public class square_pyramid {
             }
         }
 
+    }
+
+    public static void print_pattern_2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
