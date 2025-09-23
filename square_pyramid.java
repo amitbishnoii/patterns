28c0549177893caf47f0d2aadad22c758ce3e35f
amitbishnoii <amitbishnoi29012@gmail.com>
2025-09-23 22:00:01 +0530

    add more print methods to print square pyramid patterns

--- square_pyramid.java
+++ square_pyramid.java
@@ -3,6 +3,8 @@ public class square_pyramid {
     public static void main(String[] args) {
         // print_pattern(10);
         print_pattern_2(5);
+        print_pattern_3(5);
+        print_pattern_4(4);
     }
 
     public static void print_pattern(int n) {
@@ -29,6 +31,24 @@ public class square_pyramid {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void print_pattern_3(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(j);
+            }
+            System.out.println();            
+        }
+    }
+
+    public static void print_pattern_4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(i);
             }
             System.out.println();
         }
